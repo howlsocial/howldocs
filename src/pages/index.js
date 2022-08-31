50727cc184f591ea64305f06a10db7cbb91ac747
Alex Lynham <alex@lynh.am>
2022-08-31 10:57:23 +0100

    fix title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Howl Guides and Documentation ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
